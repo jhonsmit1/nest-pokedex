112bda760fb28968855935c1ad0b819d763e5f8e
jhonsmit1 <realtube565@gmail.com>
2024-11-26 21:25:14 -0500

    final commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,9 +17,9 @@ import { JoiValidationSchema } from './config/joi.validation';
       load:[envConfiguration],
       validationSchema:JoiValidationSchema
     }),
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'public'),
-    }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname, '..', 'public'),
+    // }),
 
     MongooseModule.forRoot(process.env.MONGODB,{
       dbName:'pokemonsdb'
